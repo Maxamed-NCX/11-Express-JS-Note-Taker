8233ced7d8d79b8d6f73382c0d8578f9b0d23e51
Mcnoor <99060667+Mcnoor@users.noreply.github.com>
2022-05-22 21:55:04 -0500

    adding

---
+++ .history/Develop/server_20220522210636.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+
+const app = express();
+
+
+const notes = require('./data/animals');
+
+
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
+
+app.get('/banaa', (req,res ) => {
+ res.send('Hi')
+} )
