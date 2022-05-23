51e477ca93fc0e93bbfb0c1852b44319d3c9eda3
Mcnoor <99060667+Mcnoor@users.noreply.github.com>
2022-05-23 00:06:25 -0500

    adding updates

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
-
+const path = require('path')
 const app = express();
 
 
 const notes = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
-
+app.use(express.static('public'));
 app.listen(PORT, () => {
   console.log(`API server now on port 3001!`);
 });
+
+app.get('/', (req,res ) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+} )
+
+app.get('/notes', (req,res ) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+} )
 
 app.get('/api/notes', (req,res ) => {
   console.log(req.query)
@@ -24,3 +32,5 @@ app.get('/api/notes/:id', (req,res ) => {
   console.log(notes)
  res.json(result);
 } )
+
+
